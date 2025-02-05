d544bfcc63fcc84adc93b74a350fae77d890e88b
Mostafa <mostafa.seyed88@gmail.com>
2025-02-05 17:42:43 +0100

    first commit

---
+++ Library/Main.cpp
@@ -0,0 +1,43 @@
+#include "Library.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+
+	while (true) {
+
+		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+		cout << "welcome to the library!" << endl;
+		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" << endl;
+
+		cout << "Choose from the menu: " << endl;
+		cout << "1) borrow" << endl;
+		if (true)
+		{
+
+		}
+		cout << "\n--> " << endl;
+
+		cout << "--> " << endl;
+
+		if (true)
+		{
+
+		}
+
+
+		cout << "2) Return" << endl;
+		cout << "3) See your LoanInfo" << endl;
+		cout << "4) Quit" << endl;
+		cout << "Bye, and welcome back anytime!" << endl;
+
+
+		return 0;
+	}
+}
